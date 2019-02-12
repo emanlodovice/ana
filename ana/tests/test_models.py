60c4d177e252138b4fb4b08de895f0f43d7c9ba9
Aldrin Navarro <aldrinnavarro16@gmail.com>
2019-02-12 15:57:48 +0800

    Feature/setup pytest (#18)
    * Add pytest & pytest-django
    
    * Add initial test models
    
    * Ignore vim swp files
    
    * Add pytest config
    
    * Setup travis to use pytest as test runner

---
+++ ana/tests/test_models.py
@@ -0,0 +1,10 @@
+from ana.models import Verb
+from django.test import TestCase
+
+
+class VerbModelTestCase(TestCase):
+
+    def test_model_str_repr(self):
+        verb = Verb.objects.create(slug='page-hit')
+        self.assertEqual(str(verb), verb.slug)
+
