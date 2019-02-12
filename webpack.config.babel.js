2a484aa759cbc08691d478e5743c4cbcb4a90d1e
Arnelle Balane <arnellebalane@gmail.com>
2019-02-12 17:45:21 +0800

    Move ana static assets inside ana/source directory

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,10 +1,10 @@
 import path from 'path';
 
-const STATIC_URL = process.env.STATIC_URL || 'http://localhost:5000/'
+const STATIC_URL = process.env.STATIC_URL || 'http://localhost:5000/';
 const NODE_ENV = process.env.NODE_ENV || 'production';
 
 function resolvePath(staticPath) {
-    return path.join(__dirname, 'static', staticPath);
+    return path.join(__dirname, 'ana/static/ana', staticPath);
 }
 
 const javascriptRegex = /\.jsx?$/;
@@ -13,10 +13,10 @@ const imagesRegex = /\.(jpe?g|png|gif|svg)$/;
 const fontsRegex = /\.(ttf|otf|woff2?)$/;
 
 export default {
-    entry: resolvePath('ana/index.jsx'),
+    entry: resolvePath('source/index.jsx'),
 
     output: {
-        path: resolvePath('build/ana'),
+        path: resolvePath('dist'),
         filename: 'index.js',
         chunkFilename: '[name].js',
         publicPath: STATIC_URL
