a8b28ea5fc995551c0356ff865202188dcea7fd6
Arnelle Balane <arnellebalane@gmail.com>
2019-02-13 00:50:50 +0800

    Feature/Initial react project setup (#19)
    * Define STATIC_ROOT settings
    
    * Ignore STATIC_ROOT directory (static)
    
    * Add package.json file
    
    * Add react packages to dependencies
    
    * Ignore node_modules directory
    
    * Update dashboard django template
    
    * Add initial react entrypoint script
    
    * Add webpack and loader packages to devDependencies
    
    * Add babel packages to devDependencies
    
    * Add .editorconfig
    
    * Add .babelrc
    
    * Add npm scripts for running webpack builds
    
    * Replace react-loader with babel-loader
    
    * Add @babel/preset-react to devDependencies
    
    * Add @babel/preset-react to .babelrc
    
    * Add webpack config
    
    * Read STATIC_URL setting value from env variable
    
    * Add node-sass to devDependencies
    
    * Define App.jsx and global styles
    
    * Change file-loader `name` option
    
    * Add eslint packages to devDependencies
    
    * Add eslint config and lint npm script
    
    * Add husky and lint-staged to devDependencies
    
    * Add husky and lint-staged configs in package.json
    
    * Undo changes in settings variables
    
    * Move ana static assets inside ana/source directory
    
    * Ignore dist directory inside ana/static
    
    * Change script src to frontend entrypoint script
    
    * Remove output.publichPath from webpack config
    
    It is difficult to predict what the static/public url is gonna be
    without asking the user to set some configs. We'll just try on our end
    to avoid lazyloading any components.

---
+++ webpack.config.babel.js
@@ -0,0 +1,40 @@
+import path from 'path';
+
+const NODE_ENV = process.env.NODE_ENV || 'production';
+
+function resolvePath(staticPath) {
+    return path.join(__dirname, 'ana/static/ana', staticPath);
+}
+
+const javascriptRegex = /\.jsx?$/;
+const stylesheetsRegex = /\.scss$/;
+const imagesRegex = /\.(jpe?g|png|gif|svg)$/;
+const fontsRegex = /\.(ttf|otf|woff2?)$/;
+
+export default {
+    entry: resolvePath('source/index.jsx'),
+
+    output: {
+        path: resolvePath('dist'),
+        filename: 'index.js',
+        chunkFilename: '[name].js'
+    },
+
+    mode: NODE_ENV === 'production' ? 'production' : 'development',
+
+    module: {
+        rules: [{
+            test: javascriptRegex,
+            loader: 'babel-loader'
+        }, {
+            test: stylesheetsRegex,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+        }, {
+            test: [imagesRegex, fontsRegex],
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]'
+            }
+        }]
+    }
+};
