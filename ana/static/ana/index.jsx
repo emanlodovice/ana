a1d798bee2f0ad6a7b4284cd12ce37fb8522f5fa
Arnelle Balane <arnellebalane@gmail.com>
2019-02-12 15:45:50 +0800

    Add initial react entrypoint script

---
+++ ana/static/ana/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <h1>Ana Dashboard</h1>
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#app')
+);
