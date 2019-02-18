296ee8a2537cc74a8bb4ac34d2efa39ee999bf56
Arnelle Balane <arnellebalane@gmail.com>
2019-02-18 23:32:46 +0800

    Redraw line chart when component resizes

--- ana/static/ana/source/charts/LineChart/LineChart.jsx
+++ ana/static/ana/source/charts/LineChart/LineChart.jsx
@@ -1,17 +1,20 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import * as d3 from 'd3';
 import style from './LineChart.scss';
 
 export default function LineChart({data}) {
-    const ref = React.createRef();
+    const ref = useRef();
 
-    useEffect(() => {
+    const draw = ({width, height}) => {
         const x = d3.scaleLinear()
             .domain([0, Math.max(...data.map(d => d.data.length)) - 1])
-            .range([0, 700]);
+            .range([0, width]);
         const y = d3.scaleLinear()
-            .domain([0, Math.max(...data.map(d => Math.max(...d.data)))])
-            .range([0, 200]);
+            .domain([
+                Math.min(...data.map(d => Math.min(...d.data))),
+                Math.max(...data.map(d => Math.max(...d.data)))
+            ])
+            .range([0, height]);
 
         const lineGenerator = d3.line()
             .x((d, i) => x(i))
@@ -19,20 +22,31 @@ export default function LineChart({data}) {
 
         d3.select(ref.current)
             .selectAll('path')
+            .remove();
+
+        d3.select(ref.current)
+            .select('svg')
+            .selectAll('path')
             .data(data)
             .enter()
             .append('path')
             .attr('stroke', 'red')
-            .attr('stroke-width', 3)
+            .attr('stroke-width', 1)
             .attr('d', d => lineGenerator(d.data));
+    };
+
+    useEffect(() => {
+        draw(ref.current.getBoundingClientRect());
+
+        const observer = new ResizeObserver(([entry]) => draw(entry.contentRect));
+        observer.observe(ref.current);
+
+        return () => observer.unobserve(ref.current);
     }, []);
 
     return (
-        <svg
-            ref={ref}
-            className={style.lineChart}
-            viewBox="0 0 700 200"
-        >
-        </svg>
+        <div ref={ref} className={style.lineChart}>
+            <svg></svg>
+        </div>
     );
 }
