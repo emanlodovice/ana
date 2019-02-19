423e92396c7327c15fd1503b10f61adbe18e7453
Arnelle Balane <arnellebalane@gmail.com>
2019-02-19 11:21:30 +0800

    Render LineChart svg using React instead of D3

--- ana/static/ana/source/charts/LineChart/LineChart.jsx
+++ ana/static/ana/source/charts/LineChart/LineChart.jsx
@@ -1,44 +1,42 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import * as d3 from 'd3';
 import style from './LineChart.scss';
+
+function lineGeneratorFactory({data, width=0, height=0}) {
+    const x = d3.scaleLinear()
+        .domain([0, Math.max(...data.map(d => d.data.length)) - 1])
+        .range([0, width]);
+    const y = d3.scaleLinear()
+        .domain([
+            Math.min(...data.map(d => Math.min(...d.data))),
+            Math.max(...data.map(d => Math.max(...d.data)))
+        ])
+        .range([0, height]);
+
+    return d3.line().x((d, i) => x(i)).y(d => y(d));
+}
 
 export default function LineChart({data}) {
     const ref = useRef();
 
-    const draw = ({width, height}) => {
-        const x = d3.scaleLinear()
-            .domain([0, Math.max(...data.map(d => d.data.length)) - 1])
-            .range([0, width]);
-        const y = d3.scaleLinear()
-            .domain([
-                Math.min(...data.map(d => Math.min(...d.data))),
-                Math.max(...data.map(d => Math.max(...d.data)))
-            ])
-            .range([0, height]);
+    const [lineGenerator, setLineGenerator] = useState(
+        () => lineGeneratorFactory({data})
+    );
 
-        const lineGenerator = d3.line()
-            .x((d, i) => x(i))
-            .y(d => y(d));
-
-        d3.select(ref.current)
-            .selectAll('path')
-            .remove();
-
-        d3.select(ref.current)
-            .select('svg')
-            .selectAll('path')
-            .data(data)
-            .enter()
-            .append('path')
-            .attr('stroke', 'red')
-            .attr('stroke-width', 1)
-            .attr('d', d => lineGenerator(d.data));
-    };
+    const updateLineGenerator = rect => setLineGenerator(
+        () => lineGeneratorFactory({data, ...rect})
+    );
 
     useEffect(() => {
-        draw(ref.current.getBoundingClientRect());
+        const element = ref.current;
 
-        const observer = new ResizeObserver(([entry]) => draw(entry.contentRect));
+        // Sets line generator based on initial component dimensions
+        updateLineGenerator(element.getBoundingClientRect().toJSON());
+
+        // Update line generator as the component is resized
+        const observer = new ResizeObserver(
+            ([entry]) => updateLineGenerator(entry.contentRect.toJSON())
+        );
         observer.observe(ref.current);
 
         return () => observer.unobserve(ref.current);
@@ -46,7 +44,16 @@ export default function LineChart({data}) {
 
     return (
         <div ref={ref} className={style.lineChart}>
-            <svg></svg>
+            <svg>
+                {data.map(d => (
+                    <path
+                        key={d.label}
+                        stroke="red"
+                        strokeWidth="1"
+                        d={lineGenerator(d.data)}
+                    />
+                ))}
+            </svg>
         </div>
     );
 }
