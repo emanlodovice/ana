64f79b7543363210b1607b593feef30074fd4763
Arnelle Balane <arnellebalane@gmail.com>
2019-02-19 12:01:35 +0800

    Define useLine React hook for the LineChart component

--- ana/static/ana/source/charts/LineChart/LineChart.jsx
+++ ana/static/ana/source/charts/LineChart/LineChart.jsx
@@ -1,29 +1,9 @@
-import React, {useEffect, useRef, useState} from 'react';
-import lineFactory from './lineFactory';
+import React from 'react';
+import {useLine} from './reactHooks';
 import style from './LineChart.scss';
 
 export default function LineChart({data}) {
-    const ref = useRef();
-    const [line, setLine] = useState(() => lineFactory({data}));
-
-    const updateLine = rect => setLine(
-        () => lineFactory({data, ...rect})
-    );
-
-    useEffect(() => {
-        const element = ref.current;
-
-        // Sets line generator based on initial component dimensions
-        updateLine(element.getBoundingClientRect().toJSON());
-
-        // Update line generator as the component is resized
-        const observer = new ResizeObserver(
-            ([entry]) => updateLine(entry.contentRect.toJSON())
-        );
-        observer.observe(ref.current);
-
-        return () => observer.unobserve(ref.current);
-    }, data);
+    const [ref, line] = useLine(data);
 
     return (
         <div ref={ref} className={style.lineChart}>
